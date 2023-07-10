9769597bb5d04808571044923c3ca8b8aca67a72
tiborkovaljov <tibor.k@numfil.com>
2023-07-10 19:48:17 +0200

    2016 Day 01 completed

--- 2016/Day01/Day01.cpp
+++ 2016/Day01/Day01.cpp
@@ -3,6 +3,27 @@
 #include <string>
 #include <fstream>
 #include <unordered_map>
+#include <unordered_set>
+
+struct Coordinate {
+	int x;
+	int y;
+
+	bool operator==(const Coordinate& other) const {
+		return x == other.x && y == other.y;
+	}
+};
+
+namespace std {
+	template <>
+	struct hash<Coordinate> {
+		std::size_t operator()(const Coordinate& coord) const {
+			std::size_t h1 = std::hash<int>{}(coord.x);
+			std::size_t h2 = std::hash<int>{}(coord.y);
+			return h1 ^ (h2 << 1);
+		}
+	};
+}
 
 int part1();
 
@@ -70,5 +91,47 @@ int part2() {
 		data.push_back(element);
 	}
 
-	return 0;
+	int result = 0;
+
+	std::unordered_map<int, std::pair<int, int>> orientationMap = {
+			{0, {0, 1}},
+			{1, {1, 0}},
+			{2, {0, -1}},
+			{3, {-1, 0}}
+	};
+
+	int x = 0, y = 0;
+	int orientation = 0;
+	std::unordered_set<Coordinate> visitedLocations = {};
+	bool foundTwice = false;
+
+	for (const auto& instruction : data) {
+		if (instruction[0] == 'R') {
+			orientation = (orientation + 1) % 4;
+		} else if (instruction[0] == 'L') {
+			orientation = (orientation - 1 + 4) % 4;
+		}
+
+		std::pair<int, int> move = orientationMap[orientation];
+		int distance = std::stoi(instruction.substr(1));
+
+		for (int i = 0; i < distance; ++i) {
+			x += move.first;
+			y += move.second;
+
+			Coordinate currentLocation{x, y};
+
+			if (visitedLocations.count(currentLocation) > 0) {
+				result = std::abs(x) + std::abs(y);
+				foundTwice = true;
+				break;
+			}
+
+			visitedLocations.insert(currentLocation);
+		}
+
+		if (foundTwice) { break; }
+	}
+
+	return result;
 }
