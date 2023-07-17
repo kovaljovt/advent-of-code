618e4eb6566e3882d5d6700baf346a6089c81a6f
tiborkovaljov <tibor.k@numfil.com>
2023-07-17 15:49:23 +0200

    2022 Day 04 completed

--- 2022/Day04/Day04.cpp
+++ 2022/Day04/Day04.cpp
@@ -15,11 +15,65 @@ int main() {
 }
 
 int part1() {
+	std::fstream input(R"(C:\Users\Acer\Desktop\advent-of-code\2022\Day04\Day04.txt)");
+	std::string line;
+	std::vector<std::string> data = {};
 
-	return 0;
+	while (std::getline(input, line)) {
+		data.push_back(line);
+	}
+
+	int result = 0;
+
+	for (const auto& entry: data) {
+		size_t commaPosition = entry.find(",");
+		std::string first = entry.substr(0, commaPosition);
+		std::string second = entry.substr(commaPosition+1);
+		size_t dashPosition = first.find('-');
+		int firstStart = std::stoi(first.substr(0, dashPosition));
+		int firstEnd = std::stoi(first.substr(dashPosition+1));
+		dashPosition = second.find('-');
+		int secondStart = std::stoi(second.substr(0, dashPosition));
+		int secondEnd = std::stoi(second.substr(dashPosition+1));
+
+		if ((firstStart <= secondStart) && (firstEnd >= secondEnd))
+			result++;
+		else if ((secondStart <= firstStart) && (secondEnd >= firstEnd))
+			result++;
+	}
+
+	return result;
 }
 
 int part2() {
+	std::fstream input(R"(C:\Users\Acer\Desktop\advent-of-code\2022\Day04\Day04.txt)");
+	std::string line;
+	std::vector<std::string> data = {};
 
-	return 0;
+	while (std::getline(input, line)) {
+		data.push_back(line);
+	}
+
+	int result = 0;
+
+	for (const auto& entry: data) {
+		size_t commaPosition = entry.find(",");
+		std::string first = entry.substr(0, commaPosition);
+		std::string second = entry.substr(commaPosition+1);
+		size_t dashPosition = first.find('-');
+		int firstStart = std::stoi(first.substr(0, dashPosition));
+		int firstEnd = std::stoi(first.substr(dashPosition+1));
+		dashPosition = second.find('-');
+		int secondStart = std::stoi(second.substr(0, dashPosition));
+		int secondEnd = std::stoi(second.substr(dashPosition+1));
+
+		if ((firstStart >= secondStart && firstStart <= secondEnd) ||
+		    (firstEnd >= secondStart && firstEnd <= secondEnd) ||
+		    (secondStart >= firstStart && secondStart <= firstEnd) ||
+		    (secondEnd >= firstStart && secondEnd <= firstEnd)) {
+			result++;
+		}
+	}
+
+	return result;
 }
--- 2022/Day04/Day04.py
+++ 2022/Day04/Day04.py
@@ -1,16 +1,41 @@
-with open("Day04.txt") as file:
-    data = [i for i in file.read().split()]
+def main():
+    print(part1())
+    print(part2())
 
-result = 0
+def part1():
+    with open("Day04.txt") as file:
+        data = [i for i in file.read().split()]
 
-for entry in data:
-    first, second = entry.split(",")
-    first = [int(i) for i in first.split("-")]
-    second = [int(i) for i in second.split("-")]
-    if first[0] <= second[0] and first[1] >= second[1]:
-        result += 1
-    elif second[0] <= first[0] and second[1] >= first[1]:
-        result += 1
+    result = 0
 
+    for entry in data:
+        first, second = entry.split(",")
+        first = [int(i) for i in first.split("-")]
+        second = [int(i) for i in second.split("-")]
+        if first[0] <= second[0] and first[1] >= second[1]:
+            result += 1
+        elif second[0] <= first[0] and second[1] >= first[1]:
+            result += 1
 
-print(result)
+    return result
+
+def part2():
+    with open("Day04.txt") as file:
+        data = [i for i in file.read().split()]
+
+    result = 0
+
+    for entry in data:
+        first, second = entry.split(",")
+        first = [int(i) for i in first.split("-")]
+        second = [int(i) for i in second.split("-")]
+        if first[0] in range(second[0], second[1]+1) or\
+                first[1] in range(second[0], second[1]+1):
+            result += 1
+        elif second[0] in range(first[0], first[1]+1) or\
+                second[1] in range(first[0], first[1]+1):
+            result += 1
+
+    return result
+
+main()
