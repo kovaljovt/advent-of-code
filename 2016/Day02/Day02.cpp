d2b2b593a309ebfec655178e490f375c1ab5dece
tiborkovaljov <tibor.k@numfil.com>
2023-07-23 11:28:04 +0200

    2016 Day 02 preparation

--- 2016/Day02/Day02.cpp
+++ 2016/Day02/Day02.cpp
@@ -15,11 +15,25 @@ int main() {
 }
 
 int part1() {
+	std::fstream input(R"(C:\Users\Acer\Desktop\advent-of-code\2016\Day02\Day02.txt)");
+	std::string line;
+	std::vector<std::string> data = {};
+
+	while (std::getline(input, line)) {
+		data.push_back(line);
+	}
 
 	return 0;
 }
 
 int part2() {
+	std::fstream input(R"(C:\Users\Acer\Desktop\advent-of-code\2016\Day02\Day02.txt)");
+	std::string line;
+	std::vector<std::string> data = {};
+
+	while (std::getline(input, line)) {
+		data.push_back(line);
+	}
 
 	return 0;
 }
