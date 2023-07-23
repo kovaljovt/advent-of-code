7a45aa1546d8b53a0f5a755f53519b91786cf88b
tiborkovaljov <tibor.k@numfil.com>
2023-07-23 11:26:10 +0200

    2016 Day 02 completed

---
+++ 2016/Day02/Day02_2016.py
@@ -0,0 +1,11 @@
+def main():
+    print(part1())
+    print(part2())
+
+def part1():
+    with open("Day02.txt") as file:
+        data = [i for i in file.read().split()]
+
+def part2():
+    with open("Day02.txt") as file:
+        data = [i for i in file.read().split()]
