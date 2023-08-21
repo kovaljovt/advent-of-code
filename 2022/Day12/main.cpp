00556a1c24146b5ebc1c2a30fa1c75f6a3466887
tiborkovaljov <tibor.k@numfil.com>
2023-08-21 10:05:48 +0200

    Link rework

--- 2022/Day12/main.cpp
+++ 2022/Day12/main.cpp
@@ -1,15 +1,33 @@
 #include "Day12.h"
 
 int main(int argc, char** argv) {
+    // reading link
     std::string path = std::string(argv[argc-1]);
+
+    // checking if file exists
     if (!std::filesystem::exists(path)) {
         std::cout << "File not opened!" << '\n';
         return -1;
     }
+
 	Day12 solver;
+    std::string inputData;
+
+    if (std::filesystem::is_regular_file(path)) {
+        std::ifstream input(path);
+        std::ostringstream buffer;
+        buffer << input.rdbuf();
+        inputData = buffer.str();
+    } else {
+        std::cerr << "Invalid file!" << '\n';
+        return 1;
+    }
+
+    // calling solve method
 	int part1 = solver.solve(Solution::part1, path);
 	int part2 = solver.solve(Solution::part2, path);
 	std::cout << part1 << '\n';
 	std::cout << part2 << '\n';
+
 	return 0;
 }
